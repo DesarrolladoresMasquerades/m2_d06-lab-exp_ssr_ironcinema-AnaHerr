58de57e611cbb73fb249b0624643cc365495f1d3
AnaHerr <ana.herrera.91@gmail.com>
2022-02-12 20:09:35 +0100

    lab done commit

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,36 @@
 const express = require('express');
 const router = express.Router();
 
+// require the Drone model here
+const Movie = require("../models/Movie.model");
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+router.get("/movies", (req, res, next) => {
+    Movie.find()
+    .then(movies =>{
+      console.log(`Listed ${movies.length} movies from the DB`)
+      res.render("movies", {movies})
+    })
+    .catch((err)=> console.log ("DB error reading '/movies"));
+  });
+
+
+  //ITERATION 4
+  router.get("/movies/:id", (req, res, next) => {
+    const id= req.params.id
+      Movie.findById(id)
+      .then((movie) =>{
+        res.render("movies-details", movie);
+      })
+  });
+
+
+
+
+
+
+
+
 
 module.exports = router;
